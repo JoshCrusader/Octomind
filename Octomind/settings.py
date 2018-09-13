01ef272cf2ec3f0845d3aaa7e30c257af8c1738f
nandarionndr1 <leebet16@gmail.com>
2018-09-13 23:28:18 +0800

    just added something

--- Octomind/settings.py
+++ Octomind/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'octo_site'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
