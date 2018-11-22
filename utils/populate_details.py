411a3af25957f64fb8300898e52ef3c918a092bb
JoshCrusader <joshua_cruzada@dlsu.edu.ph>
2018-11-23 00:04:53 +0800

    Popluated db

--- utils/populate_details.py
+++ utils/populate_details.py
@@ -10,6 +10,14 @@ def gen_random(x):
 def gen_r_random(x, y):
     return x + math.ceil(random.random() * (y - x))
 
+def gen_clues(chance, dts, te, game):
+    if(gen_random(100) <= chance):
+        z = gen_r_random(1, 3)
+        for i in range(0, z):
+            cd = ClueDetails(detail = 'No Detail avail', timestamp = dts+timedelta(minutes = gen_r_random(6, te-2), seconds = gen_r_random(0, 58)) )
+            cd.save()
+            clue = Clues(clue_details_id = cd.clue_details_id, game_id = game.game_id)
+            clue.save()
 def main_start():
     games = Game.objects.filter(room_id = 2)
     for game in games:
@@ -20,25 +28,34 @@ def main_start():
             detail.solved = 1
             r2 = gen_random(100)
             te = 60
-            clues = []
             if(r2 <= 5):
                 te = gen_r_random(20, 35)
-                if(gen_random(100) <= 5):
-                    z = gen_r_random(1, 3)
-                    for i in range(0, z):
-                        pass
-                        # clue = Clues(cl)
+                gen_clues(5, dts, te, game)
             elif r2 <= 20:
                 te = gen_r_random(35, 45)
+                gen_clues(5, dts, te, game)
             else:
-                te = gen_r_random(45, 60)
+                te = gen_r_random(45, 58)
+                gen_clues(40, dts, te, game)
 
-            delta_time = timedelta(minutes = te, seconds = gen_r_random(0, 60))
+            delta_time = timedelta(minutes = te, seconds = gen_r_random(0, 58))
             timee = dts + delta_time
             
             detail.timeend = timee
-            pass
+            detail.save()
         else:
-            # Lose
-            pass
-    pass
+            t3 = gen_random(100)
+            te = 60
+
+            delta_time = timedelta(minutes = te)
+            if(t3 <= 7):
+                te = gen_r_random(15, 50)
+                gen_clues(58, dts, te, game)
+                delta_time = timedelta(minutes = te, seconds = gen_r_random(0, 58))
+                timee = dts + delta_time
+                detail.timeend = timee
+                detail.save()
+            else:
+                gen_clues(40, dts, te, game)
+            
+    print('done')
