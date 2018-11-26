65d8c859cf85122f8e7b4f0808c3bc9718fe46de
nandarionndr1 <leebet16@gmail.com>
2018-11-26 22:15:47 +0800

    wawski

--- sensordb_daemon.py
+++ sensordb_daemon.py
@@ -25,7 +25,7 @@ def on_message(client, userdata, message):
     conn.close()
     pass
 
-broker_address = "192.168.100.56"
+broker_address = "172.16.12.104"
 client = mqtt.Client("P1")  # create new instance
 client.on_message = on_message  # attach function to callback
 client.connect(broker_address)  # connect to broke
